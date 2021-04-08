634365e001847f6cb57a458522dc75c587598a1a
Arya Sah <aryasah30@gmail.com>
2021-04-08 13:40:28 +0530

    hello

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_ROOT ='os.path.join(BASE_DIR, "static")'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGGING = { 
     "version":1 ,
